72e8f1e990659a175abf2805d2196d7ed3efdf0c
1straze <75583822+1straze@users.noreply.github.com>
2021-12-28 14:34:08 +0100

    Add files via upload

--- src/exit.tsx
+++ src/exit.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="exit" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <properties>
+  <property name="tilesetCopyright" value="cc0"/>
+ </properties>
  <image source="assets/worldExit.png" width="64" height="64"/>
  <tile id="0">
   <properties>
